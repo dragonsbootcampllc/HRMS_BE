9c9669d24ab04da0c020455ee08e52bed9dec863
Ahmed Tarek Radwan (ATR) <100035760+ahmedradwan21@users.noreply.github.com>
2024-05-18 19:16:19 +0300

    Update views.py

--- recruiters/views.py
+++ recruiters/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.response import Response
-from rest_framework import status
 from recruiters.models import Recruiter, Question, Category, JobPost, Applied \
         ,Application, Interview,Application
-from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 from recruiters import serializers
 from applicants import serializers as aplicantSerializers
-from rest_framework import generics
-from rest_framework import generics, mixins
-from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 
 import core.Utils as Utils
-
+from django.contrib.auth import login
+from .serializers import UserSerializer, UserLoginSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics, status, mixins
+from rest_framework.response import Response
 # Create your views here.
 
-# post a job 
+# post a job
 class GetCreatePostJob(generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin):
     serializer_class = serializers.JobPostSerializer
 
@@ -40,7 +38,7 @@ class GetCreatePostJob(generics.GenericAPIView, mixins.CreateModelMixin, mixins.
                     raise NotFound("ther is no applications")
             except KeyError as e:
                 return Response({"application": ["This field is required."]}, status=status.HTTP_400_BAD_REQUEST)
-            
+
             app = Application.objects.create(recruiter=recruiter)
 
             for ques in data['application']:
@@ -49,7 +47,7 @@ class GetCreatePostJob(generics.GenericAPIView, mixins.CreateModelMixin, mixins.
             data['application'] = app.id
         else:
             data['application'] = None
-        
+
         data['recruiter'] = recruiter_id
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -66,7 +64,7 @@ class GetCreatePostJob(generics.GenericAPIView, mixins.CreateModelMixin, mixins.
 
     def get(self, request):
         return self.list(request)
-    
+
 class GetJobInterviews(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = serializers.InterviewSerializer
 
@@ -79,7 +77,7 @@ class GetJobInterviews(generics.GenericAPIView, mixins.ListModelMixin):
 
     def get(self, request,job_id):
         return self.list(request,job_id)
-    
+
 # show all the applicants for a job
 class ShowApplicants(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = aplicantSerializers.ApplicantSerializer
@@ -91,7 +89,7 @@ class ShowApplicants(generics.GenericAPIView, mixins.ListModelMixin):
 
     def get(self, request, pk):
         return self.list(request, pk)
-    
+
     def list(self, request, *args, **kwargs):
         data = self.get_queryset()
         applicants = []
@@ -99,18 +97,18 @@ class ShowApplicants(generics.GenericAPIView, mixins.ListModelMixin):
             applicants.append(self.serializer_class(d.applicant).data)
 
         return Response(applicants, status=status.HTTP_200_OK)
-    
+
 # approve an applicant
 class UpdateApplicantStats(generics.UpdateAPIView):
     serializer_class = serializers.AppliedSerializer
     queryset = Applied.objects.all()
-    
+
     def partial_update(self, request, *args, **kwargs):
         applied = self.get_object()
         data = request.data.copy()
         if data.get('approved') is None:
             return Response({"approved": ["This field is required."]}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         applied.approved = data['approved']
         serializer = self.get_serializer(applied, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -139,30 +137,30 @@ class ScheduleInterview(generics.CreateAPIView,generics.UpdateAPIView):
         data = self.request.data.copy()
         if Interview.objects.filter(applied=applied_job).exists():
             return Response({"message": ["Interview already scheduled for this applicant"]}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         try:
             if data['date'] is None:
                 raise NotFound("This field is required.")
         except NotFound as e:
             return Response({"date": ["This field is required."]}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         data['applied'] = applied_job.id
 
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
 
         return self.create(serializer)
-    
+
     def partial_update(self, request,applied_id):
         interview = self.get_object()
         data = request.data.copy()
 
         if not isinstance(interview, Interview):
             return Response({"message": ["Interview not found"]}, status=status.HTTP_404_NOT_FOUND)
-        
+
         if data.get('date') is not None:
             if interview.date == Utils.parse_datetime_with_utc_timezone(data.get('date')):
-                return Response({"message": ["Interview already scheduled for this date"]}, status=status.HTTP_400_BAD_REQUEST) 
+                return Response({"message": ["Interview already scheduled for this date"]}, status=status.HTTP_400_BAD_REQUEST)
             interview.date = data.get('date')
 
         if data.get('cancelled') is not None:
@@ -182,19 +180,19 @@ class ScheduleInterview(generics.CreateAPIView,generics.UpdateAPIView):
             result['cancelled'] = interview.cancelled
             result['message'].append(f"Interview cancelled successfully")
 
-        # TODO: Handle no body fileds 
+        # TODO: Handle no body fileds
         return Response(result, status=status.HTTP_200_OK)
     def perform_update(self, serializer):
         instance = serializer.instance
         serializer.save()
         return instance
-    
+
 # show all the interviews of job applied to a recruiter
 class ShowInterviews(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = serializers.InterviewSerializer
     queryset = Interview.objects.all()
 
-    # get all interviews 
+    # get all interviews
     def get(self, request):
         return self.list(request)
 
@@ -202,10 +200,10 @@ class GetAllInterviews(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = serializers.InterviewSerializer
     queryset = Interview.objects.all()
 
-    # get all interviews 
+    # get all interviews
     def get(self, request):
         return self.list(request)
-    
+
 # show all the questions
 class ShowQuestions(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = serializers.QuestionSerializer
@@ -218,7 +216,7 @@ class ShowQuestions(generics.GenericAPIView, mixins.ListModelMixin):
         showingData = list(set(showingData))
         print(showingData)
         return Response(showingData,status=status.HTTP_200_OK)
-    
+
 
 # CRUD methods
 
@@ -226,15 +224,15 @@ class ShowQuestions(generics.GenericAPIView, mixins.ListModelMixin):
 class GetUsers(generics.ListAPIView):
     serializer_class = serializers.UserSerializer
     queryset = User.objects.all()
-    
+
 class GetUesr(generics.RetrieveAPIView):
     serializer_class = serializers.UserSerializer
-    
+
     def get(self, request, pk):
         user = User.objects.get(id=pk)
         serializer = self.get_serializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
 #get categories & add category
 class GetCreateCategory(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = serializers.CategorySerializer
@@ -247,8 +245,8 @@ class GetCreateCategory(generics.GenericAPIView, mixins.ListModelMixin, mixins.C
     def post(self, request):
         return self.create(request)
 
-    
-# show all recruiters 
+
+# show all recruiters
 class GetCreateRecruiters(generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin ):
     serializer_class = serializers.RecruiterSerializer
 
@@ -257,56 +255,49 @@ class GetCreateRecruiters(generics.GenericAPIView, mixins.CreateModelMixin, mixi
 
     def get(self, request):
         return self.list(request)
-    
+
     def post(self, request):
         return self.create(request)
 
-from rest_framework import generics, status, views
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .models import UserProfile
-from .serializers import UserProfileSerializer
-from rest_framework.permissions import IsAuthenticated
-from django.core.mail import send_mail
-from django.conf import settings
-class RegisterView(generics.CreateAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
 
-    def create(self, request, *args, **kwargs):
+
+
+from django.contrib.auth import login
+from .serializers import UserSerializer, UserLoginSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+class UserRegistrationAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class UserLoginAPIView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        user = serializer.validated_data
+        login(request, user)
+        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
 
-class LoginView(views.APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+from rest_framework import filters
+class EditUserAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
 
-class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
 
-class UserProfileListView(generics.ListAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
+class DeleteUserAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
 
-class SendEmailView(views.APIView):
-    permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        subject = request.data.get('subject')
-        message = request.data.get('message')
-        recipient_list = [request.data.get('recipient')]
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
-        return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+class SearchUserAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email', 'userprofile__user_address', 'userprofile__user_role']
+
+    def get_queryset(self):
+        return User.objects.all()
